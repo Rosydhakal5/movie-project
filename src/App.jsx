9c57fdf6d04795d09028d202e06d2bb08a15c410
Rosy <rosydhakal333@gmail.com>
2024-01-09 20:56:17 +1100

    Added react-bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
-import React from 'react'
+import React from 'react';
 import "./App.css"
+
+
 
 const App = () => {
   return (
     <>
-    <h1> Vite + react </h1>
+    <h1> My Movie Collection </h1>
     </>
   )
 }
 
-export default App
+export default App;
