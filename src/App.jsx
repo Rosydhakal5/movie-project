774c3a7b92bb76bc678f8425a5c13eb47ab09975
Rosy <rosydhakal333@gmail.com>
2024-01-09 21:04:27 +1100

    Added Input and Button

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import "./App.css"
-
+import SearchForm from './components/SearchForm';
 
 
 const App = () => {
   return (
     <>
     <h1> My Movie Collection </h1>
+    <SearchForm />
     </>
   )
 }
---
+++ src/components/SearchForm.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Col, Row } from 'react-bootstrap';
+
+const SearchForm = () => {
+  return (
+    <div className='bg-black p-5 rounded shadow-lg'>
+        <Row>
+            <Col>
+            Input
+            </Col>
+            <Col> Button </Col>
+        </Row>
+        </div>
+  )
+}
+
+export default SearchForm;
