f570b6c57030ba059fb21636db2878215ab20de0
Rosy <rosydhakal333@gmail.com>
2024-01-09 21:08:17 +1100

    Added Search bar with Button using React Bootstrap

--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
-import { Col, Row } from 'react-bootstrap';
+import { Col, Row, Button, Form } from 'react-bootstrap';
 
 const SearchForm = () => {
   return (
     <div className='bg-black p-5 rounded shadow-lg'>
         <Row>
             <Col>
-            Input
+            <Form.Control placeholder = "Search by title" />
             </Col>
-            <Col> Button </Col>
+            <Col> <Button> Search Movie</Button> </Col>
         </Row>
         </div>
   )
