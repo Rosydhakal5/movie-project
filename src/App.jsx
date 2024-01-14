0fd3edb8abf2ba11c763d815ed7749418c194f87
Rosy <rosydhakal333@gmail.com>
2024-01-14 23:41:06 +1100

    few changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,8 @@
 import { Col, Container, Row } from "react-bootstrap";
 import "./App.css";
 import SearchForm from "./components/SearchForm";
-import { Display } from "./components/Display";
-import { useState } from "react";
 
 function App() {
-  const [movieList, setMovieList] = useState([]);
-
-  const addMoveToList = (movie) => {
-    const tempArg = movieList.filter((itme) => itme.imdbID !== movie.imdbID);
-    setMovieList([...tempArg, movie]);
-  };
-
-  const handleOnDelete = (imdbID) => {
-    if (window.confirm("Are you sure you want to delete?")) {
-      setMovieList(movieList.filter((item) => item.imdbID !== imdbID));
-    }
-  };
 
   return (
     <>
@@ -28,8 +14,8 @@ function App() {
           </Col>
         </Row>
         <hr />
-        <SearchForm addMovieToList={addMoveToList} />
-        <Display movieList={movieList} handleOnDelete={handleOnDelete} />
+        <SearchForm  />
+        {/* <Display movieList={movieList} handleOnDelete={handleOnDelete} /> */}
       </Container>
     </>
   );
--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -1,44 +1,13 @@
-// import { Alert } from "bootstrap";
-import { useRef, useState } from "react";
 import { Button, Col, Form, Row } from "react-bootstrap";
-import { fetchMovies } from "../helper/fetchMovies";
-import { CustomCard } from "./CustomCard";
 
-const SearchForm = ({ addMovieToList }) => {
-  // fetch("https://tmdb.api.com/movies?apiKey=df2343");
-  const strRef = useRef("");
-  const [searchedMovie, setSearchedMovie] = useState({});
-
-  const handleOnSubmit = async (e) => {
-    e.preventDefault();
-
-    const str = strRef.current.value;
-    alert(str);
-
-    const data = await fetchMovies(str);
-    console.log(data);
-
-    alert(JSON.stringify(data));
-
-    if (data.Response === "True") {
-      setSearchedMovie(data);
-    } else {
-      // setError(true);
-    }
-  };
-
-  const func = (mode) => {
-    addMovieToList({ ...searchedMovie, mode });
-    setSearchedMovie({});
-    strRef.current.value = "";
-  };
+const SearchForm = () => {
 
   return (
     <div className="bg-black p-5 rounded shadow-lg">
-      <Form onSubmit={handleOnSubmit}>
+      <Form >
         <Row>
           <Col>
-            <Form.Control ref={strRef} placeholder="Search by title" />
+            <Form.Control placeholder="Search by title" />
           </Col>
           <Col>
             <Button variant="warning" type="submit">
@@ -50,9 +19,9 @@ const SearchForm = ({ addMovieToList }) => {
       <div className="d-flex justify-content-center mt-5">
         {/* {error && <Alert variant="danger">Movie not found!</Alert>}
          */}
-        {searchedMovie.imdbID && (
+        {/* {searchedMovie.imdbID && (
           <CustomCard searchedMovie={searchedMovie} func={func} />
-        )}
+        )} */}
       </div>
     </div>
   );
